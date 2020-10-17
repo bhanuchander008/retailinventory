5fe33d530233df95d873ccf7743f7cf8bf8481cf
bhanuchander008 <34958449+bhanuchander008@users.noreply.github.com>
2020-10-17 18:09:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,31 +14,10 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://bhanu:bhanu@1015@localhost/retail"
 db = SQLAlchemy(app)
 
-cur_dir = os.getcwd()
 
-# selecting app environment
-if os.getenv("ENV") == "test":
-    db_url = "sqlite:////"+cur_dir+"/temp/test.db"
-    port = 5000
-    host = "0.0.0.0"
-
-elif os.getenv("ENV") == "development":
-    db_url = "sqlite:////"+cur_dir+"/temp/development.db"
-    port = 5000
-    host = "0.0.0.0"
-    
-elif os.getenv("ENV") == "demo":
-    db_url = "sqlite:////"+cur_dir+"/temp/demo.db"
-    port = 5000
-    host = "0.0.0.0"
-
-elif os.getenv("ENV") == "production":
-    db_url = 'sqlite:////"+cur_dir+"/temp/production.db'
-    port = 5000
-    host = "0.0.0.0"
-    
 
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
